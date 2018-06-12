f3f25e0096b87651c1c55ba7f7ee815987557e86
Tyler Anger <tyler@tylers-macbook-pro.local>
2018-06-12 14:14:46 -0400

    Changed score type from Int to Double.  This way users can add custom log levels between the standard values. Spaced out score values for more room of custom values Also added any log level for use on logger objects.  this way it will allow logging of any level.

--- Sources/Logger/LogLevel.swift
+++ Sources/Logger/LogLevel.swift
@@ -9,34 +9,38 @@ import Foundation
 
 public struct LogLevel {
     
-    public static let debug = LogLevel(1, name: "debug", symbol: "✳️")
-    public static let info = LogLevel(2, name: "info", symbol: "ℹ️")
-    public static let warn = LogLevel(3, name: "warn", symbol: "⚠️")
-    public static let error = LogLevel(4, name: "error", symbol: "🚫")
-    public static let fatal = LogLevel(5, name: "fatal", symbol: "🆘")
-    public static let none = LogLevel(6, name: "none")
+    private static let MIN_LOG_LEVEL: Double = 0.0
+    private static let MAX_LOG_LEVEL: Double = 600.0
+    public static let any = LogLevel(MIN_LOG_LEVEL, name: "any")
+    public static let debug = LogLevel(100, name: "debug", symbol: "✳️")
+    public static let info = LogLevel(200, name: "info", symbol: "ℹ️")
+    public static let warn = LogLevel(300, name: "warn", symbol: "⚠️")
+    public static let error = LogLevel(400, name: "error", symbol: "🚫")
+    public static let fatal = LogLevel(500, name: "fatal", symbol: "🆘")
+    public static let none = LogLevel(MAX_LOG_LEVEL, name: "none")
     
-    private let score: Int
+    private let score: Double
     public let name: String
     public let STDName: String
     public let symbol: String?
     
-    private init(_ score: Int, name: String, STDName: String, symbol: String?) {
+    public init(_ score: Double, name: String, STDName: String, symbol: String?) {
+        precondition(score >= LogLevel.MIN_LOG_LEVEL && score <= LogLevel.MAX_LOG_LEVEL, "Log level score must be greater than or equal to \(LogLevel.any.score) and less than or equal to \(LogLevel.none.score)")
         self.score = score
         self.name = name
         self.STDName = STDName
         self.symbol = symbol
     }
     
-    private init(_ score: Int, name: String, STDName: String) {
+    public init(_ score: Double, name: String, STDName: String) {
         self.init(score, name: name, STDName: STDName, symbol: nil)
     }
     
-    private init(_ score: Int, name: String) {
+    public init(_ score: Double, name: String) {
         self.init(score, name: name, STDName: name.uppercased())
     }
     
-    private init(_ score: Int, name: String, symbol: String) {
+    public init(_ score: Double, name: String, symbol: String) {
         self.init(score, name: name, STDName: name.uppercased(), symbol: symbol)
     }
 }
